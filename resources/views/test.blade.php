4f8d3ab68b576ce6f856f9c7d452374f0d26cc56
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-05 07:31:40 +1100

    Created basic Laravel project and a test route

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+ <html>
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>Document</title>
+
+  </head>
+  <body>
+      <h1>You have reached the test page!</h1>
+  </body>
+</html>
