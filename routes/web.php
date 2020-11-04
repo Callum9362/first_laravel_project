55f1c30e6b8f7318dc0de97b197dbb867971ca74
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-05 08:08:01 +1100

    Added a wildcard route to return a blog post based on a collection array

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Example Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-   //Returns a name from a query parameter
-
-   Route::get('/', function () {
-       $name = request('name);
-       return name;
-    });
-
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,5 +12,23 @@ Route::get('/test', function () {
 
     return view('test', [
         'name' => $name
+    ]);
+});
+
+//Wildcard route
+Route::get('/posts/{post}', function ($post) {
+
+    //Data collection as there is no db at this time.
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post',
+        'my-second-post' => 'This is my second post!'
+    ];
+
+    if(! array_key_exists($post, $posts)){
+        abort(404, 'Sorry that post was not found');
+    }
+
+    return view('post', [
+        'post' => $posts[$post]
     ]);
 });
