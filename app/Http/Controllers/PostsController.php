3003c13591d2b5d09b2cf44a117c77f146b3cd62
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-05 08:39:18 +1100

    Refactored posts controller to access the db posts table

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,19 +3,13 @@
 namespace App\Http\Controllers;
 
 class PostsController {
-    public function show($post){
-        //Data collection as there is no db at this time.
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post',
-        'my-second-post' => 'This is my second post!'
-    ];
+    public function show($slug){
 
-    if(! array_key_exists($post, $posts)){
-        abort(404, 'Sorry that post was not found');
-    }
+
+    $post = \DB::table('posts')->where('slug', $slug)->first();
 
     return view('post', [
-        'post' => $posts[$post]
+        'post' => $post
     ]);
     }
 }
