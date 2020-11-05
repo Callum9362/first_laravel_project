e3c52c4294b6ba0d88a5beab1a605ec6ccabc624
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-05 15:38:11 +1100

    Added the basic 7 crud operations to the articles controller

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,15 +7,41 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
+
+    //Renders a particular article
     public function show($id){
         $article = Article::find($id);
-
         return view('articles.show', ['article' => $article]);
     }
 
+    //Render a list of articles
     public function index(){
         $articles = Article::latest()->get();
-
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    //Renders a page to create articles
+    public function create(){
+
+    }
+
+    //Renders a page to persist articles from the create form.
+    public function store(){
+
+    }
+
+    //Renders a page to edit articles
+    public function edit(){
+
+    }
+
+    //Persists an edit to an article
+    public function update(){
+
+    }
+
+    // destroy an article
+    public function destroy(){
+
     }
 }
